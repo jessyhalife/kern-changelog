2e42a87b6f68f68a4a1786239f241f5a3e29cfff
Jessy Halife <jesica.halife@kern-it.com.ar>
2020-04-14 16:53:53 -0300

    Updates

---
+++ precache-manifest.13891b56521b0c2e625e1a74d76035dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3ae6ac53b5d6d63da17acf42066b85",
+    "url": "/kern-changelog/index.html"
+  },
+  {
+    "revision": "1268f1ee1f1c0faf3f76",
+    "url": "/kern-changelog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7636d31576c8599d05f1",
+    "url": "/kern-changelog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1268f1ee1f1c0faf3f76",
+    "url": "/kern-changelog/static/js/2.73855847.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/kern-changelog/static/js/2.73855847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7636d31576c8599d05f1",
+    "url": "/kern-changelog/static/js/main.90fdc6d0.chunk.js"
+  },
+  {
+    "revision": "96b6cb010cc1da30a740",
+    "url": "/kern-changelog/static/js/runtime-main.7d6cc8c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kern-changelog/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/const.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-  "KernWebPatientDoctor.API": "master", 
-  "KernLis": "release_1.4.5.X", 
-  "KERN_MIC_PLUS": "release-1.0.x",
-  "KernIX": "master", 
-  "KernValidadorOnline": "master"
-}
