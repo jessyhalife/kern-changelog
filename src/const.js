2def983ab96a1d0531356d1f1b27985036495cf1
Jessy Halife <jesica.halife@kern-it.com.ar>
2020-04-24 09:59:05 -0300

    release 1 6

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 module.exports =  {
   "KernWebPatientDoctor.API": "master", 
-  "KernLis": "release_1.4.5.X", 
+  "KernLis": "release_1.4.6.X", 
   "KERN_MIC_PLUS": "release-1.0.x",
   "KernIX": "master", 
   "KernValidadorOnline": "master"
